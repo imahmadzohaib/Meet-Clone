ace5cd10f0ee71cae0c516963d46bd1f4691a963
Ahmad Zohaib <imahmadzohaib@gmail.com>
2026-08-31 11:52:57 +0500

    first commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,23 +1,13 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="column items-center">
-      <img
-        alt="Quasar logo"
-        src="~@/assets/quasar-logo-vertical.svg"
-        style="width: 200px; height: 200px"
-      />
-
-      <q-btn
-        class="q-mt-md"
-        color="primary"
-        to="/second"
-        label="Go to Second Page"
-        no-caps
-      />
-    </div>
-  </q-page>
+  <div>
+    
+  </div>
 </template>
 
-<script setup>
-//
+<script lang="ts" setup>
+
 </script>
+
+<style>
+
+</style>
